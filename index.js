37283a5103974a97903b7a629cc779c43522c872
Joshua Woods <nordev@protonmail.com>
2022-05-26 22:21:49 -0500

    setup of files

---
+++ index.js
@@ -0,0 +1,43 @@
+//When the user presses the key, your code will randomly place two different ships in two separate locations on the board. Each ship is only 1 unit long (In the real game ships are 2+ in length).
+var readlineSync = require('readline-sync');
+
+//var
+var prompt1= null;
+//array
+
+//classes=> objects
+  //create a object for the ship(then a function the recreats them)
+   // will (...spread) these randomllly into a array 
+//player 
+  //?keep score
+
+//functions
+//Step One
+  // const placeShip () => "
+    //(when a (promtone) is filled crate random ships in array )"";
+//Step two
+ 
+
+//logic
+//Step One
+console.log('Press any key to start the game');
+var promt1 = readlineSync.prompt('');
+// Wait for user's response. Should create a random area of map
+//placeShip();
+var userName = readlineSync.question('Enter a location to strike ie ');
+  //will use the check method to compare IF its in the array
+
+  //const operation = readlineSync.question(
+  //will use the limit I understand it limits user to what I set
+
+  //'Please enter a operation  (+, -, *, /): ',
+  //{limit: math, //limit will check the math array if wrong it displays the error
+  //limitMessage: "This is not a valid operation"
+ // }
+
+  
+ //Step two
+
+//do the check in the array
+ 
+//if not{please try again}
